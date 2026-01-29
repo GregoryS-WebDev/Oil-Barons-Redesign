b17e9ee4dc32b6c3868b265c080293f44b2aa36d
Gregory Scego <gregory.j.scego@gmail.com>
2026-01-29 16:11:41 -0600

    Filled out file structure and updated internal links

---
+++ src/pages/schedule/schedule.js
@@ -0,0 +1,23 @@
+/*====================================================================================================*/
+/* META INFORMATION */
+/*====================================================================================================*/
+
+/*
+
+    ** FILE STRUCTURE **
+
+    This javascript file is separated into logical sections, with subsection headers integrated within. The start
+    of these sections are denoted by comments such as:
+
+    /*=============================================================*/
+    /* SECTION NAME */
+    /*=============================================================*\
+
+    The end of each primary section is marked with a comment such as: /* END ROOT STYLES *\
+    Subsections have a single header comment such as:
+
+    /*-- subsection name ------------------------------------------*\
+
+*/
+
+/* END META INFORMATION */
